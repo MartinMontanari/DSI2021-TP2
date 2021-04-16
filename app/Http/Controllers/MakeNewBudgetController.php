7d0ecfbe63b3b22aa37b8d46cd65e1bceedb8bd5
Martin Montanari <martinmontanari3@gmail.com>
2021-04-16 18:33:53 -0300

    .

--- app/Http/Controllers/MakeNewBudgetController.php
+++ app/Http/Controllers/MakeNewBudgetController.php
@@ -2,25 +2,81 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Budget;
+use App\Models\BuildingMaterial;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class MakeNewBudgetController extends Controller
 {
 
-
     public function __invoke(Request $request)
     {
 
-        $customerId = $request->input('customer_id');
+        $customerId = $request->input('customerId');
 
-        if(!is_null($customerId)){
-            $customer = Customer::query()->where('id','=', $customerId)->get()->first();
-            if(!isset($customer))
-            {
-                throw new \RuntimeException('El cliente no existe', 400);
-            }
-            $constructionSite = $customer->getBuildingUnderConstruction();
+        if (is_null($customerId)) {
+            throw new \InvalidArgumentException('El cliente ingresado es incorrecto.', 400);
         }
+        try {
+            $customer = $this->findCustomerByIdOrFail($customerId);
+
+            $layerThickness = $request->input('layerThickness');
+            $materialId = $request->input('materialId');
+
+            if(is_null($materialId)){
+                throw new \InvalidArgumentException('El cliente ingresado es incorrecto.', 400);
+            }
+            $material = $this->findMaterialByIdOrFail($materialId);
+
+            $createdBudget = $this->createBudget($customer, $layerThickness, $material);
+
+        } catch (\InvalidArgumentException $error) {
+            return redirect()->back()->withErrors($error->getMessage());
+        }
+    }
+
+    /**
+     * @param int $customerId
+     * @return Customer|null
+     */
+    private function findCustomerByIdOrFail(int $customerId): ?Customer
+    {
+        $query = Customer::query()->where('id', '=', $customerId)->get()->first();
+
+        if (!isset($query)) {
+            throw new \RuntimeException('El cliente no existe.', 404);
+        }
+
+        return $query;
+    }
+
+    /**
+     * @param int $materialId
+     * @return BuildingMaterial|null
+     */
+    private function findMaterialByIdOrFail(int $materialId): ?BuildingMaterial
+    {
+        $query = BuildingMaterial::query()->where('id', '=', $materialId)->get()->first();
+
+        if (!isset($query)) {
+            throw new \RuntimeException('El cliente no existe.', 404);
+        }
+
+        return $query;
+    }
+
+    /**
+     * @param Customer $customer
+     * @param int $layerThickness
+     * @return Budget
+     */
+    private function createBudget(Customer $customer, int $layerThickness): Budget
+    {
+        $budget = new Budget();
+
+        $budget->setCustomer($customer);
+        $budget->setLayerThickness($layerThickness);
+
     }
 }
