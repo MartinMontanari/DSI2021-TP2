604a6817d2525c7d70cb2f6ccd083c37f7b09053
Martín Montanari <martinmontanari3@gmail.com>
2021-08-02 17:38:03 -0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[\App\Http\Controllers\RegisterParticipantsAndInscriptionsController::class,'__invoke']);
+Route::get('/',function(){
+    return 'hola';
+});
